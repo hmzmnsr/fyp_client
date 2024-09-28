de1fc096230d1ea5b8dad9cf69a7d778b2edd83a
Hamza <hamzamansoora@gmail.com>
2024-09-29 02:57:22 +0500

    initial commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,8 +8,7 @@ const Navbar = () => {
 
   return (
     <div className='grid grid-cols-12 bg-primary-color w-full py-6'>
-      <div className='col-span-1'></div>
-      <div className="col-span-2 flex justify-center items-center"><img src={logo} alt='logo' className='h-16' /></div>
+      <div className="col-span-3 flex justify-center items-center"><img src={logo} alt='logo' className='h-16' /></div>
       <div className='col-span-1 flex justify-center items-center'><NavLink to="/" className={() => "py-3 px-4 text-xl text-white relative after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[4px] after:bg-secondary-color after:transition-all after:duration-300 after:transform after:-translate-x-1/2 hover:after:w-full hover:after:scale-x-100"}><span>Home</span></NavLink></div>
       <div className='col-span-1 flex justify-center items-center'><NavLink to="/programs" className={() => "py-3 px-4 text-xl text-white relative after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[4px] after:bg-secondary-color after:transition-all after:duration-300 after:transform after:-translate-x-1/2 hover:after:w-full hover:after:scale-x-100"}><span>Programs</span></NavLink></div>
       <div className='col-span-1 flex justify-center items-center'><NavLink to="/faculty" className={() => "py-2 px-4 text-xl text-white relative after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[4px] after:bg-secondary-color after:transition-all after:duration-300 after:transform after:-translate-x-1/2 hover:after:w-full hover:after:scale-x-100"}><span>Faculty</span></NavLink></div>
@@ -46,12 +45,11 @@ const Navbar = () => {
         </div>
 
       </div>
-      <div className='col-span-1 flex justify-center items-center'>
+      <div className='col-span-2 flex justify-center items-center'>
         <a href="https://portal-usa-edu.com/" target="_blank" rel="noopener noreferrer" className='px-10 py-4 bg-secondary-color hover:bg-red-700 text-white rounded-2xl transition duration-300 ease-in-out'>
           Portal
         </a>
       </div>
-      <div className='col-span-1'></div>
     </div>
 
   );
