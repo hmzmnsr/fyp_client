e38a07ec9479bcb02c6286c37364fc8b846e41ff
Hamza <hamzamansoora@gmail.com>
2024-10-03 02:07:33 +0500

    initial commit

--- src/pages/faculty/banner.jsx
+++ src/pages/faculty/banner.jsx
@@ -12,29 +12,28 @@ const FacultyBanner = () => {
   }, []);
 
   return (
-    <div
-      className={`relative transform transition-transform duration-500 ${
-        isVisible ? "translate-x-0" : "translate-x-full opacity-0"
-      }`}
-    >
-      {/* Image with fade effect and overlay */}
-      <div className="relative w-full">
-        <img
-          src={facultybanner}
-          alt="faculty"
-          className="w-full h-screen object-cover"
-        />
-        <div
-          className="absolute inset-0 bg-gradient-to-r from-black/90 to-transparent"
-        ></div>
+    <div className="overflow-hidden">
+      <div
+        className={`relative transform transition-transform duration-500 ${
+          isVisible ? "translate-x-0" : "translate-x-full opacity-0"
+        }`}
+      >
+        {/* Image with fade effect and overlay */}
+        <div className="relative w-full">
+          <img
+            src={facultybanner}
+            alt="faculty"
+            className="w-full h-screen object-cover"
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-black/90 to-transparent"></div>
 
-        {/* Text content on the left side */}
-        <div className="absolute inset-0 flex justify-start items-center pr-10 z-10 h-screen ml-40">
-          <div className="text-center bg-primary-color w-28 h-3/6">
-            <div className="text-8xl text-white font-semibold py-4 px-6">
-              Computer Science Department's Faculty
-            </div>
-            <div className="text-6xl text-white font-semibold mt-5 py-4 px-6"> 
+          {/* Text content on the left side */}
+          <div className="absolute inset-0 flex justify-start items-center pr-10 z-10 h-screen ml-40">
+            <div className="text-center bg-primary-color w-28 h-3/6">
+              <div className="text-8xl text-white font-semibold py-4 px-6">
+                Computer Science Department's Faculty
+              </div>
+              <div className="text-6xl text-white font-semibold mt-5 py-4 px-6"></div>
             </div>
           </div>
         </div>
