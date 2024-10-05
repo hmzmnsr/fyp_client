ff8bd1137b49d9dace8c03578e01bf5e6116d163
Hamza <hamzamansoora@gmail.com>
2024-10-06 01:37:41 +0500

    initial commit

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GalleryBanner from './banner';
+import GalleryIntro from './intro';
 
 const Gallery = () => {
     return (
         <div className='mb-40'>
             <GalleryBanner />
+            <GalleryIntro />
         </div>
     )
 }
---
+++ src/pages/gallery/intro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GalleryIntro = () => {
+    return (
+        <div className="mx-20 my-10 px-10 py-10">
+            <div className="text-primary-color text-4xl font-bold text-center">Welcome to the Computer Science Department Gallery - University of South Asia</div>
+            <div className="text-primary-color text-2xl text-center my-5 leading-10">Explore the vibrant journey of the Computer Science Department at the University of South Asia through our gallery. From innovative research projects and groundbreaking seminars to student achievements and collaborative events, witness the dynamic spirit of our academic community. Here, we celebrate the passion, creativity, and accomplishments of our students, faculty, and alumni who continue to push the boundaries of technology and innovation. Dive into our visual collection and experience the heartbeat of computer science at USA!</div>
+        </div>
+    )
+}
+
+export default GalleryIntro;
